24467dfe44faf147ce31ea609c1e28ddcaaf942d
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-06-22 23:32:59 -0500

    header test

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import Header from '../../components/Header';
+
+describe('<Header />', () => {
+  test('It should render component header', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.length).toEqual(1);
+  });
+  test('It should render title', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
