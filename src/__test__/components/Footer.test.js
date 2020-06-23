89dc8f8818df373f1b60638d85abddb07d73c5b6
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-06-23 08:33:42 -0500

    snapshot

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
-describe('', () => {
+describe('<Footer />', () => {
   const footer = mount(<Footer />);
 
   test('It should render of component footer', () => {
@@ -10,5 +11,12 @@ describe('', () => {
   });
   test('It should render of title', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Footer snapshot', () => {
+  test('It should Check the UI component footer', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
