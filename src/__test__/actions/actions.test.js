b10891c000249a72d36b193c7c0eab097ef800b2
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-06-23 08:43:05 -0500

    challenge

--- src/__test__/actions/actions.test.js
+++ src/__test__/actions/actions.test.js
@@ -10,4 +10,12 @@ describe('Actions', () => {
     };
     expect(actions.addToCart(payload)).toEqual(expected);
   });
+  test('It should removeFromCart Action', () => {
+    const payload = ProductMock;
+    const expected = {
+      type: 'REMOVE_FROM_CART',
+      payload,
+    };
+    expect(actions.removeFromCart(payload)).toEqual(expected);
+  });
 });
